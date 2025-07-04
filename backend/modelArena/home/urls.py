66125fcec79c274905f9553ec0a02a7002786227
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-07-04 07:56:06 +0530

    fix: minor fixes

--- backend/modelArena/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import JWTTokenObtainPairView, RegisterAPIView
-
-
-urlpatterns = [
-    path('token/', JWTTokenObtainPairView.as_view()),
-    path('register/', RegisterAPIView.as_view()),
-]
