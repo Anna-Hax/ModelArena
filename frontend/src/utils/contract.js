290972588a694b2739ec5a02bac635c80f2fbbf1
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-07-04 11:41:29 +0530

    fix: backend fixes

--- frontend/src/utils/contract.js
+++ frontend/src/utils/contract.js
@@ -1,7 +1,7 @@
 import * as ethers from "ethers";
 import ArenaABI from "../abis/Arena.json";
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 
 export const getArenaContract = async () => {
   if (!window.ethereum) throw new Error("Wallet not found");
